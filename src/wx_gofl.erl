5ab1575baea2e139db4c6677231dc0723e864b6e
Michal Jurek <jurmcc@gmail.com>
2023-10-10 23:25:21 +0200

    Tick button trigers evaluation of new generation

--- src/wx_gofl.erl
+++ src/wx_gofl.erl
@@ -1,5 +1,8 @@
 -module(wx_gofl).
 
+%temp exports
+-export([neighours/2]).
+
 -export([start_link/1, tick/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
@@ -8,6 +11,9 @@
 -include_lib("wx/include/wx.hrl").
 
 -record(state, {wx_env, dim, check_boxes}).
+
+-define(ID_TICK, 1).
+-define(ID_PLAY, 2).
 
 %%% API
 
@@ -29,14 +35,23 @@ init([Dim]) ->
     MainSizer = wxBoxSizer:new(?wxVERTICAL),
     ToolsSizer = wxBoxSizer:new(?wxHORIZONTAL),
     wxSizer:add(MainSizer, ToolsSizer, [{proportion, 1}, {flag, ?wxEXPAND}]),
-    StartButton = wxButton:new(Frame, ?wxID_ANY, [{label, "Start"}]),
-    wxSizer:add(ToolsSizer, StartButton, [{proportion, 1}, {flag, ?wxEXPAND}]),
+    TickButton = wxButton:new(Frame, ?ID_TICK, [{label, "Tick"}]),
+    PlayButton = wxButton:new(Frame, ?ID_PLAY, [{label, "Play"}]),
+    wxSizer:add(ToolsSizer, TickButton, [{proportion, 1}, {flag, ?wxEXPAND}]),
+    wxSizer:add(ToolsSizer, PlayButton, [{proportion, 1}, {flag, ?wxEXPAND}]),
+
+    wxEvtHandler:connect(TickButton, command_button_clicked),
 
     CheckBoxesWithCoords = lists:foldl(fun(X, Acc) ->
                                                RowSizer = wxBoxSizer:new(?wxHORIZONTAL),
                                                wxSizer:add(MainSizer, RowSizer, [{proportion, 1}, {flag, ?wxEXPAND}]),
-                                               NewCheckBoxes = [{X,Y, wxCheckBox:new(Frame, ?wxID_ANY, "")} || Y <- lists:seq(1,Dim)],
-                                               lists:map(fun({_,_, ChBox}) ->
+                                               NewCheckBoxes = [{{X,Y}, wxCheckBox:new(Frame, ?wxID_ANY, "")} || Y <- lists:seq(1,Dim)],
+                                               lists:map(fun({{_,_}, ChBox}) ->
+                                                                 Checked = case rand:uniform(2) of
+                                                                               1 -> true;
+                                                                               2 -> false
+                                                                           end,
+                                                                 wxCheckBox:setValue(ChBox, Checked),
                                                                  wxSizer:add(RowSizer, ChBox, [{proportion, 1}, {flag, ?wxEXPAND}])
                                                          end,
                                                          NewCheckBoxes),
@@ -53,32 +68,57 @@ init([Dim]) ->
 
     gen_server:cast(self, tick),
 
-    {ok, #state{wx_env = WxEnv, dim = Dim, check_boxes = CheckBoxesWithCoords}}.
+    {ok, #state{wx_env = WxEnv, dim = Dim, check_boxes = maps:from_list(CheckBoxesWithCoords)}}.
 
-handle_cast(tick, #state{dim = Dim, check_boxes = CheckBoxes} = State) ->
-    AfterEvaluation = lists:map(fun({X,Y,CheckBox}) ->
-                                        NeighboursForCoord = neighours({X,Y}, Dim),
-                                        io:format("NeighboursForCoord: {x,y}: ~p, ~p~n", [{X,Y}, NeighboursForCoord]),
-                                        {X,Y, CheckBox, wxCheckBox:isChecked(CheckBox)}
-                                end,
-                                CheckBoxes),
-    %io:format("After Evaluation: ~p~n", [AfterEvaluation]),
-    {noreply, State};
+handle_cast(tick, State) ->
+    tick(State);
 handle_cast(_Req, State) ->
     {noreply, State}.
 
 handle_call(_Req, _From, State) ->
     {noreply, State}.
 
+handle_info(#wx{event = #wxCommand{type = command_button_clicked}}, State) ->
+    tick(State);
 handle_info(#wx{event = #wxClose{}}, State) ->
+    {stop, normal, State};
+handle_info(Event, State) ->
+    io:format("Unhandled event: ~p~n", [Event]),
     {stop, normal, State}.
 
 %%% Internal functions
 
-neighours({X,Y}, Dim) ->
-    lists:filter(fun({NX, _}) when NX =< 1; NX >= Dim -> false;
-                    ({_, NY}) when NY =< 1; NY >= Dim -> false;
-                    (_) -> true
-                 end,
-                 [{X-1,Y}, {X+1,Y}, {X,Y-1}, {X,Y+1}]).
+tick(#state{dim = Dim, check_boxes = CheckBoxes} = State) ->
+    AfterEvaluation = maps:map(fun({X,Y}, CheckBox) ->
+                                       NeighboursForCoord = neighours({X,Y}, Dim),
+                                       OwnState = case wxCheckBox:getValue(CheckBox) of
+                                                      true -> alive;
+                                                      _ -> dead
+                                                  end,
+                                       NeighboursStates = lists:map(fun(Bool) ->
+                                                                            case Bool of 
+                                                                                true -> alive;
+                                                                                _ -> dead
+                                                                            end
+                                                                    end,
+                                                                    [wxCheckBox:getValue((maps:get({NX,NY}, CheckBoxes))) || {NX,NY} <- NeighboursForCoord]),
+                                       NextOwnState = game_of_life:evolve(OwnState, NeighboursStates),
+                                       %io:format("NeighboursForCoord: {x,y}: ~p, ~p~n", [{X,Y}, NeighboursForCoord]),
+                                       %io:format("NeighboursStates:  ~p~n", [NeighboursStates]),
+                                       %io:format("NextOwnState:  ~p~n", [NextOwnState]),
+                                       {CheckBox, NextOwnState}
+                               end,
+                               CheckBoxes),
+    io:format("After Evaluation: ~p~n", [AfterEvaluation]),
+    maps:map(fun(_Key, {CheckBox, BoolState}) ->
+                      case BoolState of
+                          alive -> wxCheckBox:setValue(CheckBox, true);
+                          _ -> wxCheckBox:setValue(CheckBox, false)
+                      end
+              end,
+              AfterEvaluation),
+    {noreply, State}.
+
+neighours({X,Y}, Dim) when X =< 1; X >= Dim; Y =< 1; Y >= Dim -> [];
+neighours({X,Y}, _Dim) -> [{X-1,Y}, {X+1,Y}, {X,Y-1}, {X,Y+1}].
 
